e1ba850f5d7502fa92a1f823625b75ea448deacb
Pranto Kumar <50362018+pranto113015@users.noreply.github.com>
2024-02-03 22:39:10 +0600

    Add files via upload

---
+++ Message_usingfunction.java
@@ -0,0 +1,14 @@
+package message_usingfunction;
+
+public class Message_usingfunction {
+
+    void display() {
+        System.out.println("Welcome! This Java Function");
+    }
+
+    public static void main(String[] args) {
+        Message_usingfunction ob = new Message_usingfunction();
+        ob.display();
+    }
+
+}
