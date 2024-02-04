49667d951c4b1e880e26682a70d2a335f85b4ee3
Pranto Kumar <50362018+pranto113015@users.noreply.github.com>
2024-02-05 00:17:01 +0600

    Add files via upload

---
+++ String_equals.java
@@ -0,0 +1,13 @@
+package string_equals;
+
+public class String_equals {
+
+    public static void main(String[] args) {
+        String s1 = "Pranto";
+        String s2 = "Pranto";
+        String s3 = "Kumar";
+        System.out.println(s1.equals(s2));
+        System.out.println(s1.equals(s3));
+    }
+
+}
