df4bf5db0138ff01554942da229d818e3ef0ce6b
Pranto Kumar <50362018+pranto113015@users.noreply.github.com>
2024-02-05 00:03:42 +0600

    Add files via upload

---
+++ String_concat.java
@@ -0,0 +1,11 @@
+package string_concat;
+
+public class String_concat {
+
+    public static void main(String[] args) {
+     String s1= "Pranto ";
+     s1=s1.concat( "Kumar");
+        System.out.println(s1);
+    }
+
+}
