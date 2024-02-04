0bec0f9e4d43df17728f9a98fb63674c4ec8bdaf
Pranto Kumar <50362018+pranto113015@users.noreply.github.com>
2024-02-05 00:28:00 +0600

    Add files via upload

---
+++ String_compare.java
@@ -0,0 +1,16 @@
+
+package string_compare;
+
+
+public class String_compare {
+
+   
+    public static void main(String[] args) {
+       String s1 = "Aranto";
+        String s2 = "Pranto";
+
+        System.out.println(s1.compareTo(s2));
+     
+    }
+    
+}
