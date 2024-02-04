2cb2bf3c07d801109c9b1019cc476e195d64c698
Pranto Kumar <50362018+pranto113015@users.noreply.github.com>
2024-02-04 23:46:04 +0600

    Add files via upload

---
+++ String_declare.java
@@ -0,0 +1,10 @@
+package string_declare;
+
+public class String_declare {
+
+    public static void main(String[] args) {
+        String s1 = "Hello! Java String Function";
+        System.out.println(s1);
+    }
+
+}
