90717b67c80f906fb0cacae2b6c7a46d2b87ad15
Pranto Kumar <50362018+pranto113015@users.noreply.github.com>
2024-02-04 23:54:29 +0600

    Add files via upload

---
+++ String_array.java
@@ -0,0 +1,11 @@
+package string_array;
+
+public class String_array {
+
+    public static void main(String[] args) {
+        char a[]={'P','r','a','n','t','o'};
+        String  s3=new String (a);
+        System.out.println(s3);
+    }
+
+}
