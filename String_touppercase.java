849de336a4f5089b440fe60f411106897a89f6d4
Pranto Kumar <50362018+pranto113015@users.noreply.github.com>
2024-02-05 21:23:34 +0600

    Add files via upload

---
+++ String_touppercase.java
@@ -0,0 +1,10 @@
+package string_touppercase;
+
+public class String_touppercase {
+
+    public static void main(String[] args) {
+        String s1 = "pranto kumar";
+        System.out.println(s1.toUpperCase());
+    }
+
+}
