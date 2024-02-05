dd865b4ae11766e806564771afe9d4d8b53687b9
Pranto Kumar <50362018+pranto113015@users.noreply.github.com>
2024-02-05 21:29:02 +0600

    Add files via upload

---
+++ String_Lowercase.java
@@ -0,0 +1,13 @@
+
+package string_lowercase;
+
+
+public class String_Lowercase {
+
+
+    public static void main(String[] args) {
+         String s1 = "PRANTO KUMAR";
+        System.out.println(s1.toLowerCase());
+    }
+    
+}
