3f4fdf1beaf61344da88387b7cde85228bb16943
Pranto Kumar <50362018+pranto113015@users.noreply.github.com>
2024-02-12 23:08:43 +0600

    Add files via upload

---
+++ Default_constructor.java
@@ -0,0 +1,13 @@
+package default_constructor;
+
+public class Default_constructor {
+
+    Default_constructor() {
+        System.out.println("I'm inside Constructor");
+    }
+
+    public static void main(String[] args) {
+        Default_constructor ob = new Default_constructor();
+    }
+
+}
